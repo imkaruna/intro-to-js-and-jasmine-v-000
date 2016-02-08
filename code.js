1b222d80be7437962f6d1308b01945f8bf8b4817
Karuna <imkaruna@gmail.com>
2016-02-07 19:46:02 -0500

    Done

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(iceCream) {
+  return "I love "+ iceCream;
+}
+
+function shouting(sentence) {
+  return sentence.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return 0==0;
+}
